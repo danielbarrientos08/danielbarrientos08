b27c76c2f331b2f26019faa6e63c0459a9701ffd
DanielBQ <danielbarrientos0802@gmail.com>
2022-03-17 22:37:25 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const API = "https://api.github.com/users/";
+
+async function doSearch(){
+    const response = await fetch(API+'juanwmedia')
+}
+
+const app = Vue.createApp({
+
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
+    }  
+});
+
